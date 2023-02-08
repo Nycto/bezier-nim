be3b1cd8738cd0794408b8d05005a4458003e7f8
Nycto <James@RoundEights.com>
2023-02-07 18:26:03 -0800

    Produce HTML from cli

--- src/bezier/cli.nim
+++ src/bezier/cli.nim
@@ -1,13 +1,14 @@
-import std/[parseopt, strutils], simplepng
+import std/[parseopt, strutils], strformat
 
 # Quick fix for IDE integration
 when not (compiles(Bezier)): import ../bezier, vmath
 
-var filename: string = "bezier.png"
+var filename: string = "bezier.html"
 var width: int = 500
 var height: int = 500
 var cliParser = initOptParser()
 var showExtrema = false
+var showBoundingBox = false
 const linePoints = 500
 
 var nums = newSeq[float32]()
@@ -22,33 +23,68 @@ for kind, key, val in cliParser.getopt():
         of "width", "w": width = val.parseInt
         of "height", "h": height = val.parseInt
         of "extrema", "e": showExtrema = true
+        of "boundingBox", "b": showBoundingBox = true
         else: assert(false, "Unsupported option: " & key)
     of cmdEnd: assert(false) # cannot happen
 
 assert(nums.len in [ 2, 4, 6, 8 ])
 
-proc pixel(image: var Pixels, point: Vec2): var Pixel =
-    return image[point.x.int, height - point.y.int]
+proc line(p1, p2: Vec2, color: string = "black"): string =
+    let p1y = height.float - p1.y
+    let p2y = height.float - p2.y
+    return &"""<line x1="{p1.x}" y1="{p1y}" x2="{p2.x}" y2="{p2y}" stroke="{color}" />{"\n"}"""
 
-proc drawDot(image: var Pixels, point: Vec2, r, g, b: int) =
-    image.pixel(point - vec2(1, 0)).setColor(r, g, b, 255)
-    image.pixel(point + vec2(1, 0)).setColor(r, g, b, 255)
-    image.pixel(point - vec2(0, 1)).setColor(r, g, b, 255)
-    image.pixel(point + vec2(0, 1)).setColor(r, g, b, 255)
+proc dot(center: Vec2, color: string = "red"): string =
+    let y = height.float - center.y
+    &"""<circle cx="{center.x}" cy="{y}" r="2" fill="{color}" />"""
+
+proc rect(p1, p2: Vec2, color: string): string =
+    result.add(line(vec2(p1.x, p1.y), vec2(p1.x, p2.y), color))
+    result.add(line(vec2(p1.x, p2.y), vec2(p2.x, p2.y), color))
+    result.add(line(vec2(p2.x, p2.y), vec2(p2.x, p1.y), color))
+    result.add(line(vec2(p2.x, p1.y), vec2(p1.x, p1.y), color))
 
 proc draw[N](curve: Bezier[N]) =
-    var image = initPixels(width, height)
+    var svg = ""
 
+    var previous = -1
     for t in 0..linePoints:
-        let point = curve.compute(1.0 / linePoints.float * t.float)
-        image.pixel(point).setColor(0, 0, 0, 255)
+        if previous >= 0:
+            let p1 = curve.compute(1.0 / linePoints.float * previous.float)
+            let p2 = curve.compute(1.0 / linePoints.float * t.float)
+            svg.add(line(p1, p2))
+        previous = t
 
     when compiles(curve.derivative):
         if showExtrema:
             for extrema in curve.extrema:
-                image.drawDot(curve.compute(extrema), 255, 0, 0)
+                svg.add(dot(curve.compute(extrema)))
 
-    simplePng(filename, image)
+    if showBoundingBox:
+        let box = curve.boundingBox()
+        svg.add(rect(vec2(box.minX, box.minY), vec2(box.maxX, box.maxY), "lightgreen"))
+
+    #simplePng(filename, image)
+    filename.writeFile([
+        """<!DOCTYPE html>""",
+        """<html lang="en">""",
+        """<body>""",
+        &"""<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">""",
+            """<defs>""",
+                """<pattern id="smallGrid" width="10" height="10" patternUnits="userSpaceOnUse">""",
+                    """<path d="M 10 0 L 0 0 0 10" fill="none" stroke="#ccc" stroke-width="0.5"/>""",
+                """</pattern>""",
+                """<pattern id="grid" width="100" height="100" patternUnits="userSpaceOnUse">""",
+                    """<rect width="100" height="100" fill="url(#smallGrid)"/>""",
+                    """<path d="M 100 0 L 0 0 0 100" fill="none" stroke="#aaa" stroke-width="1"/>""",
+                """</pattern>""",
+            """</defs>""",
+            """<rect width="100%" height="100%" fill="url(#grid)" />""",
+            svg,
+        """</svg>""",
+        """</body>""",
+        """</html>"""
+    ].join("\n"))
 
 let points = countup(0, nums.len - 1, 2).toSeq.mapIt(vec2(nums[it], nums[it + 1]))
 
